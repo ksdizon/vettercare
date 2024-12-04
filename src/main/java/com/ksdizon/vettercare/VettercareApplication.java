a5dc966a4082d4a2a204ccb61a62c8cd7bd4fdc1
Ken Shamrock Dizon <dizonshamrock@gmail.com>
2024-12-04 21:28:18 +0800

    Initial commit

---
+++ src/main/java/com/ksdizon/vettercare/VettercareApplication.java
@@ -0,0 +1,13 @@
+package com.ksdizon.vettercare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VettercareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VettercareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ksdizon/vettercare/VettercareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ksdizon.vettercare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VettercareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
